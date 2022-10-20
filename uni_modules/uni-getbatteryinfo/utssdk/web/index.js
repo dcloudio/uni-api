29fd7c771df4ed634a357f74abb42d56e3aaf3c6
yanyilin <yanyilin@dcloud.io>
2022-10-20 16:20:08 +0800

    add uni-getbatteryinfo

---
+++ uni_modules/uni-getbatteryinfo/utssdk/web/index.js
@@ -0,0 +1,19 @@
+export default function getBatteryInfo(options) {
+    if (navigator.getBattery) {
+        navigator.getBattery().then(battery => {
+            const res = {
+                errMsg: 'getBatteryInfo:ok',
+                level: battery.level * 100,
+                isCharging: battery.charging
+            }
+            options.success && options.success(res)
+            options.complete && options.complete(res)
+        })
+    } else {
+        const res = {
+            errMsg: 'getBatteryInfo:fail navigator.getBattery is unsupported'
+        }
+        options.fail && options.fail(res)
+        options.complete && options.complete(res)
+    }
+}
