2c15ed70b2324635ddd6880e98b0e83c772f5748
yanyilin <yanyilin@dcloud.io>
2024-06-21 19:50:22 +0800

    同步代码@20240621

---
+++ uni_modules/uni-media/utssdk/app-ios/Frameworks/DCloudMediaPicker.xcframework/ios-arm64_x86_64-simulator/DCloudMediaPicker.framework/Headers/DCloudMediaVideoCompress.h
@@ -0,0 +1,18 @@
+//
+//  DCloudMediaVideoCompress.h
+//  DCloudMediaPicker
+//
+//  Created by wangzhitong on 2024/5/17.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DCloudMediaVideoCompress : NSObject
++ (NSDictionary * _Nullable)getVideoInfo:(NSURL *)videoUrl;
++ (void)compressVideoWithVideoUrl:(NSURL *)videoUrl outputPath:(NSString *)outputPath compressComplete:(void(^)(id responseObjc))compressComplete;
++ (void)compressVideoWithVideoUrl:(NSURL *)videoUrl outputPath:(NSString *)outputPath withBiteRate:(NSNumber * _Nullable)outputBiteRate withFrameRate:(NSNumber * _Nullable)outputFrameRate withVideoWidth:(NSNumber * _Nullable)outputWidth withVideoHeight:(NSNumber * _Nullable)outputHeight compressComplete:(void(^)(id responseObjc))compressComplete;
+@end
+
+NS_ASSUME_NONNULL_END
