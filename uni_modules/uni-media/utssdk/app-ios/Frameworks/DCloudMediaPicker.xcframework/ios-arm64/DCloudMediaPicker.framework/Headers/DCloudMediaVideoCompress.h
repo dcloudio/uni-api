a3d3d5138f3aaa019b4a6d0e61f85c8216bb86de
yanyilin <yanyilin@dcloud.io>
2024-08-24 11:08:04 +0800

    同步代码@20240824

--- uni_modules/uni-media/utssdk/app-ios/Frameworks/DCloudMediaPicker.xcframework/ios-arm64/DCloudMediaPicker.framework/Headers/DCloudMediaVideoCompress.h
+++ uni_modules/uni-media/utssdk/app-ios/Frameworks/DCloudMediaPicker.xcframework/ios-arm64/DCloudMediaPicker.framework/Headers/DCloudMediaVideoCompress.h
@@ -11,8 +11,8 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface DCloudMediaVideoCompress : NSObject
 + (NSDictionary * _Nullable)getVideoInfo:(NSURL *)videoUrl;
-+ (void)compressVideoWithVideoUrl:(NSURL *)videoUrl outputPath:(NSString *)outputPath compressComplete:(void(^)(id responseObjc))compressComplete;
-+ (void)compressVideoWithVideoUrl:(NSURL *)videoUrl outputPath:(NSString *)outputPath withBiteRate:(NSNumber * _Nullable)outputBiteRate withFrameRate:(NSNumber * _Nullable)outputFrameRate withVideoWidth:(NSNumber * _Nullable)outputWidth withVideoHeight:(NSNumber * _Nullable)outputHeight compressComplete:(void(^)(id responseObjc))compressComplete;
++ (void)compressVideoWithVideoUrl:(NSURL *)videoUrl outputPath:(NSString *)outputPath compressComplete:(void(^)(NSDictionary *responseObjc))compressComplete;
++ (void)compressVideoWithVideoUrl:(NSURL *)videoUrl outputPath:(NSString *)outputPath withBiteRate:(NSNumber * _Nullable)outputBiteRate withFrameRate:(NSNumber * _Nullable)outputFrameRate withVideoWidth:(NSNumber * _Nullable)outputWidth withVideoHeight:(NSNumber * _Nullable)outputHeight compressComplete:(void(^)(NSDictionary *responseObjc))compressComplete;
 @end
 
 NS_ASSUME_NONNULL_END
